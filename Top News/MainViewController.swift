038c36efa7a99f3326b59d3635c8dfce0724e8a6
egorchernenko <lordo916@gmail.com>
2017-08-06 16:10:49 +0300

    add gui;

---
+++ Top News/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewController.swift
+//  Top News
+//
+//  Created by Egor on 06.08.17.
+//  Copyright © 2017 Egor. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
